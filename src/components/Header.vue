99de5d82cc1103011c29bb72bc766415c667c054
NathanDavisBarrett <76917233+NathanDavisBarrett@users.noreply.github.com>
2021-09-14 06:31:05 -0600

    no initial about bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -113,7 +113,7 @@ export default {
       return {
           myName: "NathanDavisBarrett",
           codeNameAttrs: ["Engineer", "Developer", "Researcher"],
-          selection: "about",
+          selection: "",
       }
   },
   methods: {
